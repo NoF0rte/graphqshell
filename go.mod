e4f8b26a31d3b09d5afd7aac335b943aa30cb8aa
NoF0rte <nof0rte@users.noreply.github.com>
2023-04-26 22:41:31 -0600

    Making more progress

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/NoF0rte/graphqshell
 
 go 1.19
 
-require github.com/google/uuid v1.3.0 // indirect
+require github.com/google/uuid v1.3.0
+
+require github.com/emirpasic/gods v1.18.1 // indirect
--- pkg/graphql/main.go
+++ pkg/graphql/main.go
@@ -2,7 +2,9 @@ package graphql
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/emirpasic/gods/stacks/arraystack"
 	"github.com/google/uuid"
 )
 
@@ -15,14 +17,42 @@ const (
 	kindEnum        string = "ENUM"
 	kindList        string = "LIST"
 
-	typeInt    string = "Int"
-	typeString string = "String"
-	typeBool   string = "Boolean"
-	typeID     string = "ID"
-	typeURI    string = "URI"
+	typeInt      string = "Int"
+	typeString   string = "String"
+	typeBool     string = "Boolean"
+	typeID       string = "ID"
+	typeURI      string = "URI"
+	typeDateTime string = "DateTime"
+	typeHTML     string = "HTML"
 )
 
-var typeMap map[string]FullType = make(map[string]FullType)
+var (
+	typeMap      map[string]FullType        = make(map[string]FullType)
+	objCache     map[string]*Object         = make(map[string]*Object)
+	deferResolve map[string][]func(*Object) = make(map[string][]func(*Object))
+	resolveStack *arraystack.Stack          = arraystack.New()
+)
+
+func getOrCreateObj(ref *TypeRef) *Object {
+	fqName := ref.String()
+	obj, ok := objCache[fqName]
+	if !ok {
+		obj = ref.Create()
+		objCache[fqName] = obj
+	}
+
+	return obj
+}
+
+func isResolving(name string) bool {
+	for _, value := range resolveStack.Values() {
+		if name == value.(string) {
+			return true
+		}
+	}
+
+	return false
+}
 
 type IntrospectionResponse struct {
 	Data struct {
@@ -57,7 +87,18 @@ type Field struct {
 	Description       string       `json:"description"`
 	IsDeprecated      bool         `json:"isDeprecated"`
 	Name              string       `json:"name"`
-	Type              TypeRef      `json:"type"`
+	Type              *TypeRef     `json:"type"`
+}
+
+func (f Field) Create() *Object {
+	var obj *Object
+	found := getOrCreateObj(f.Type)
+	if found != nil {
+		obj = found.Copy()
+		obj.Name = f.Name
+	}
+
+	return obj
 }
 
 type EnumValue struct {
@@ -71,7 +112,7 @@ type InputValue struct {
 	DefaultValue interface{} `json:"defaultValue"`
 	Description  string      `json:"description"`
 	Name         string      `json:"name"`
-	Type         TypeRef     `json:"type"`
+	Type         *TypeRef    `json:"type"`
 }
 
 type TypeRef struct {
@@ -108,19 +149,34 @@ func (t TypeRef) String() string {
 	}
 }
 
+func (t TypeRef) RootName() string {
+	if t.OfType == nil {
+		return t.Name
+	}
+
+	return t.OfType.RootName()
+}
+
 func (t TypeRef) Create() *Object {
 	switch t.Kind {
 	case kindNonNull:
-		return t.OfType.Create()
+		// check stack?
+		return getOrCreateObj(t.OfType)
 	case kindList:
+		// check stack?
+		var value []*Object
+		obj := getOrCreateObj(t.OfType)
+		if obj == nil {
+			return nil
+		}
+
+		value = append(value, obj)
 		return &Object{
-			Name: t.String(),
-			Value: []*Object{
-				t.OfType.Create(),
-			},
+			Name:  t.String(),
+			Value: value,
 		}
 	case kindInputObject:
-		var fields []Object
+		var fields []*Object
 
 		objType, ok := typeMap[t.Name]
 		if !ok {
@@ -134,10 +190,11 @@ func (t TypeRef) Create() *Object {
 			}
 
 			if field.Value == nil {
-				field.Value = inputField.Type.Create()
+				// check stack
+				field.Value = getOrCreateObj(inputField.Type)
 			}
 
-			fields = append(fields, field)
+			fields = append(fields, &field)
 		}
 
 		return &Object{
@@ -170,13 +227,57 @@ func (t TypeRef) Create() *Object {
 			obj.Value = uuid.New().String()
 		case typeURI:
 			obj.Value = "https://example.com"
-		default:
-			panic(fmt.Errorf("no default value for scalar %s", t.Name))
+		case typeDateTime:
+			obj.Value = time.Now()
+		case typeHTML:
+			obj.Value = "<html><body><h1>Example</h1></body></html>"
+		default: // Make configurable
+			fmt.Printf("[!] No default value for scalar %s\n", t.Name)
 		}
 
 		return &obj
+	case kindObject:
+		objType, ok := typeMap[t.Name]
+		if !ok {
+			panic(fmt.Errorf("unknown input object type %s", t.Name))
+		}
+
+		resolveStack.Push(t.Name)
+
+		obj := &Object{
+			Name:   t.Name,
+			Fields: make([]*Object, 0),
+		}
+
+		for _, f := range objType.Fields {
+			rootTypeName := f.Type.RootName()
+			if isResolving(rootTypeName) {
+				fmt.Printf("[+] [%s] Deferring field %s: %s\n", t.Name, f.Name, rootTypeName)
+				deferResolve[rootTypeName] = append(deferResolve[rootTypeName], func(o *Object) {
+					obj.Fields = append(obj.Fields, o)
+				})
+				continue
+			}
+
+			fmt.Printf("[+] [%s] Creating field %s: %s\n", t.Name, f.Name, rootTypeName)
+			fieldObj := f.Create()
+			if fieldObj != nil {
+				obj.Fields = append(obj.Fields, fieldObj)
+			}
+		}
+
+		resolveStack.Pop()
+
+		defered := deferResolve[t.Name]
+		for _, fun := range defered {
+			fun(obj)
+		}
+
+		delete(deferResolve, t.Name)
+
+		return obj
 	default:
-		fmt.Printf("[!] Either unknown or ignored kind %s", t.Kind)
+		fmt.Printf("[!] Either unknown or ignored kind %s\n", t.Kind)
 		return nil
 	}
 }
@@ -195,9 +296,48 @@ type Query struct {
 
 type Object struct {
 	Name   string
-	Args   []Object
-	Fields []Object
+	Args   []*Object
+	Fields []*Object
 	Value  interface{}
+}
+
+func (o *Object) Copy() *Object {
+	copied := &Object{
+		Name:   o.Name,
+		Args:   make([]*Object, len(o.Args)),
+		Fields: make([]*Object, len(o.Fields)),
+		Value:  o.Value,
+	}
+	copy(copied.Args, o.Args)
+	copy(copied.Fields, o.Fields)
+
+	return copied
+}
+
+func (o *Object) GenValue() interface{} {
+	if o.Value == nil {
+		value := make(map[string]interface{})
+		for _, field := range o.Fields {
+			value[field.Name] = field.GenValue()
+		}
+
+		return value
+	}
+
+	switch v := o.Value.(type) {
+	case *Object:
+		return v.GenValue()
+	case []*Object:
+		var values []interface{}
+		for _, obj := range v {
+			values = append(values, obj.GenValue())
+		}
+		return values
+	case EnumValue:
+		return v.Name
+	default:
+		return v
+	}
 }
 
 type RootMutation struct {
@@ -211,11 +351,24 @@ func ParseIntrospection(response IntrospectionResponse) (*RootQuery, *RootMutati
 
 	types := schema.Types
 	for _, t := range types {
-		if t.Name == "Query" {
-			for _, field := range t.Fields {
-				fmt.Println(field.Type.String())
-			}
+		typeMap[t.Name] = t
+
+		// if t.Name == "Query" {
+		// 	for _, field := range t.Fields {
+		// 		fmt.Println(field.Type.String())
+		// 	}
+		// }
+	}
+
+	queryType := typeMap["Query"]
+	for _, field := range queryType.Fields {
+		fmt.Println(field.Name)
+		obj := field.Create()
+		if obj != nil {
+			fmt.Println(obj.GenValue())
+			break
 		}
 	}
+
 	return nil, nil, nil
 }
